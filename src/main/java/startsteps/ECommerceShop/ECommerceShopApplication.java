036565449a87c99586366ffe72d41e97d6e8d840
Lobanova_Mariia <lobanovmariia@gmail.com>
2024-01-08 12:46:09 +0100

    Initial commit

---
+++ src/main/java/startsteps/ECommerceShop/ECommerceShopApplication.java
@@ -0,0 +1,13 @@
+package startsteps.ECommerceShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceShopApplication.class, args);
+	}
+
+}
