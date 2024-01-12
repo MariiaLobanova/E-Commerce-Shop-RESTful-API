a0fc2dcdb7f8e843c7cf09d0560efeed0a3958d5
Lobanova_Mariia <lobanovmariia@gmail.com>
2024-01-12 14:45:58 +0100

    ECSRA-57 added Swagger documentation to application

--- src/main/java/startsteps/ECommerceShop/ECommerceShopApplication.java
+++ src/main/java/startsteps/ECommerceShop/ECommerceShopApplication.java
@@ -3,9 +3,12 @@ package startsteps.ECommerceShop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ECommerceShopApplication {
 
+
+@SpringBootApplication
+
+
+public class ECommerceShopApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ECommerceShopApplication.class, args);
 	}
--- src/main/java/startsteps/ECommerceShop/config/SecurityConfig.java
+++ src/main/java/startsteps/ECommerceShop/config/SecurityConfig.java
@@ -30,8 +30,25 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(request->request.requestMatchers("/api/v1/auth/signup", "/api/v1/auth/signin","/api/v1/info","/api/v1/unsecured")
-                        .permitAll().anyRequest().authenticated())
+                .authorizeHttpRequests(request->request
+                        .requestMatchers
+                                ("/v2/api-docs",
+                                "/swagger-resources",
+                                "/swagger-resources/**",
+                                "/configuration/ui",
+                                "/configuration/security",
+                                "/swagger-ui.html",
+                                "/webjars/**",
+                                "/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-resources/**",
+                                "/swagger-ui/index.html").permitAll()
+                        .requestMatchers
+                                ("/api/v1/auth/signup",
+                                "/api/v1/auth/signin",
+                                "/api/v1/unsecured").permitAll()
+                        .requestMatchers(
+                                "/api/v1/info").permitAll().anyRequest().authenticated())
                 .sessionManagement(manager ->manager.sessionCreationPolicy(STATELESS))
                 .authenticationProvider(authenticationProvider()).addFilterBefore(
                         jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
