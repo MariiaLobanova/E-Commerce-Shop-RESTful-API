c1e49320cd6aceffba5c9bd4c9a2495af4816e66
Lobanova_Mariia <lobanovmariia@gmail.com>
2024-01-18 16:55:59 +0100

    ECSRA-57-Swagger Documentation- added SwaggerConfig.class and needed annotations to controllers related to swagger

--- src/main/java/startsteps/ECommerceShop/controller/AuthorizationController.java
+++ src/main/java/startsteps/ECommerceShop/controller/AuthorizationController.java
@@ -1,5 +1,7 @@
 package startsteps.ECommerceShop.controller;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
