05447df7a1f58c77cc092720cc27ff4e73b79a32
Lobanova_Mariia <lobanovmariia@gmail.com>
2024-01-19 22:09:55 +0100

    ECSRA-58-Global exception handler, convert responses to json format

---
+++ src/main/java/startsteps/ECommerceShop/exeptions/ErrorResponse.java
@@ -0,0 +1,14 @@
+package startsteps.ECommerceShop.exeptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorResponse {
+    private int status;
+    private String error;
+    private String message;
+}
--- src/main/java/startsteps/ECommerceShop/exeptions/GlobalExceptionHandler.java
+++ src/main/java/startsteps/ECommerceShop/exeptions/GlobalExceptionHandler.java
@@ -4,28 +4,49 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<Object> handleUserNotFoundException(UserNotFoundException exception){
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(exception.getMessage());
+        ErrorResponse errorResponse = new ErrorResponse(
+                HttpStatus.NOT_FOUND.value(),
+                HttpStatus.NOT_FOUND.getReasonPhrase(),
+                exception.getMessage()
+        );
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(errorResponse);
     }
     @ExceptionHandler(PasswordNotCorrectException.class)
     public ResponseEntity<Object> handlePasswordNotCorrectException(PasswordNotCorrectException exception){
+        ErrorResponse errorResponse = new ErrorResponse(
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(),
+                exception.getMessage()
+        );
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(exception.getMessage());
+                .body(errorResponse);
     }
     @ExceptionHandler(UserAlreadyExistException.class)
     public ResponseEntity<Object> handleUserAlreadyExistException(UserAlreadyExistException exception){
+        ErrorResponse errorResponse = new ErrorResponse(
+                HttpStatus.BAD_REQUEST.value(),
+                HttpStatus.BAD_REQUEST.getReasonPhrase(),
+                exception.getMessage()
+        );
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body(exception.getMessage());
+                .body(errorResponse);
     }
     @ExceptionHandler
     public ResponseEntity<Object> handleRuntimeException(RuntimeException exception) {
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(),
+                exception.getMessage()
+        );
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(exception.getMessage());
+                .body(errorResponse);
     }
 }
