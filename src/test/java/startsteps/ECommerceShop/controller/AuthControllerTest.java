388245419e38b1a27051fc12e47c029309b73826
Lobanova_Mariia <lobanovmariia@gmail.com>
2024-01-19 16:47:12 +0100

    ECSRA-55-writing tests- changed getAdminDataTest, using @WebMvcTestAnnotation and injection mockmvc by getting review on PR#5

--- src/test/java/startsteps/ECommerceShop/controller/AuthControllerTest.java
+++ src/test/java/startsteps/ECommerceShop/controller/AuthControllerTest.java
@@ -6,6 +6,10 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
@@ -15,22 +19,33 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import startsteps.ECommerceShop.entities.Role;
 import startsteps.ECommerceShop.entities.User;
 import startsteps.ECommerceShop.repository.UserRepository;
+import startsteps.ECommerceShop.service.JwtService;
+import startsteps.ECommerceShop.service.JwtServiceImpl;
+import startsteps.ECommerceShop.service.UserService;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
-@ExtendWith(MockitoExtension.class)
+@WebMvcTest(AuthorizationController.class)
 class AuthorizationControllerTest {
 
-    @Mock
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
     private UserRepository userRepository;
-
-    @InjectMocks
-    private AuthorizationController authorizationController;
+    @MockBean
+    private UserService userService;
+    @MockBean
+    private JwtService jwtService;
 
     @Test
     @WithMockUser(username = "admin", roles = "ADMIN")
@@ -40,16 +55,13 @@ class AuthorizationControllerTest {
                 new User(1L,"testname2","test2email@gmail","password", Role.USER));
         given(userRepository.findAll()).willReturn(userList);
 
-        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(authorizationController).build();
-
-        String responseContent = mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/admin"))
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/admin"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andReturn().getResponse().getContentAsString();
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].username").value("test1email@gmail"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].username").value("test2email@gmail"))
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON));
 
-        Assertions.assertNotNull(userList);
-        Assertions.assertEquals(2,userList.size());
-        Assertions.assertTrue(responseContent.contains("USER"));
-        Assertions.assertTrue(responseContent.contains("test2email@gmail"));
+        verify(userRepository, times(1)).findAll();
     }
 }
+
