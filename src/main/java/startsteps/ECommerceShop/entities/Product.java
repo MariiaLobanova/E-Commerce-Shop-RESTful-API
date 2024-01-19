43698185fc88f20a21909b35a9444d381ea8ef08
Lobanova_Mariia <lobanovmariia@gmail.com>
2024-01-19 17:45:54 +0100

    ECSRA-41-created product model

---
+++ src/main/java/startsteps/ECommerceShop/entities/Product.java
@@ -0,0 +1,34 @@
+package startsteps.ECommerceShop.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "products")
+public class Product {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long productId;
+    @NotBlank
+    @Column
+    String name;
+    @Column
+    String description;
+    @Min(0)
+    @Column
+    double price;
+    @Min(0)
+    @Column
+    int quantity;
+}
