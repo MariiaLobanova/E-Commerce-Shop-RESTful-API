9cd4892e064e8e1c87539d03aa3de5d92b926c31
Lobanova_Mariia <lobanovmariia@gmail.com>
2024-01-24 16:34:05 +0100

    ECSRA-000 deleted unuseble lines in tests

--- src/test/java/startsteps/ECommerceShop/controller/ProductControllerTest.java
+++ src/test/java/startsteps/ECommerceShop/controller/ProductControllerTest.java
@@ -2,7 +2,6 @@ package startsteps.ECommerceShop.controller;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.Page;
@@ -17,12 +16,6 @@ import startsteps.ECommerceShop.service.JwtService;
 import startsteps.ECommerceShop.service.JwtServiceImpl;
 import startsteps.ECommerceShop.service.ProductService;
 import startsteps.ECommerceShop.service.UserService;
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.Mockito.*;
@@ -37,8 +30,6 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @WebMvcTest(ProductController.class)
 @WithMockUser(username = "admin", roles = "ADMIN")
